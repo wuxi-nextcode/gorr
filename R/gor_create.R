da0ddb3cedf99b93cf669bf54ada24c24e5cd6cc
edvaldig <edvaldig@gmail.com>
2019-09-24 11:27:30 +0000

    gor_create now handles replacement as well as definitions

--- R/gor_create.R
+++ R/gor_create.R
@@ -6,6 +6,7 @@
 #' Relations can be either local dataframes (uploaded) or string queries(turned into create statements and prepended to the query when executed).
 #'
 #' @param ... ellipse for the relations to include
+#' @param defs definitions string
 #' @param conn connection object from \code{\link{gor_connect}}
 #'
 #' @return partial-application of function \code{\link{gor_query}} with the `conn` and `relations` parameters set.
@@ -17,24 +18,73 @@
 #'   query("nor [air] | where Treatment = 'nonchilled'")
 #'
 #' }
-gor_create <- function(..., conn = NULL) {
+gor_create <- function(..., defs = "", conn = NULL, replace = NULL) {
   dots <- pryr::named_dots(...)
-  relations <- lapply(dots, eval, parent.frame())
+  if (!is.null(replace)) {
+    if (class(replace) != "gor_creation") {
+        stop("replace parameter must be of class gor_creation, see gor_create", call. = F)
+    }
 
-  # Pick out virtual relations
-  virtual_relations <- purrr::keep(relations, is.data.frame)
-  # Pick out create statements
-  create_statements <- relations %>%
-    purrr::keep(is.character) %>%
-    purrr::imap(~ stringr::str_glue("create {.y} = {.x};")) %>%
-    paste(collapse = "\n")
+    prev <- attributes(replace)
+    if (is.null(conn)) {
+      conn <- prev$conn
+    }
 
-  if (interactive()) {
+    if (defs == "") {
+      defs <- prev$defs
+    }
+
+    prev_names <- discard(names(prev$dots), ~ . %in% names(dots))
+
+    dots <- c(dots, prev$dots[prev_names])
 
   }
 
-  function(query) {
-      query <- paste(create_statements, query, sep = "\n")
+  dots <- discard(dots, is.null)
+
+  fn <- function(query) {
+      # evaluating the dots inside this function is necessary, otherwise virtual relations are fixed.
+      relations <- lapply(dots, eval, parent.frame())
+      # Pick out virtual relations
+      virtual_relations <- purrr::keep(relations, is.data.frame)
+      # Pick out create statements
+      create_statements <- relations %>%
+        purrr::keep(is.character) %>%
+        purrr::imap(~ stringr::str_glue("create {.y} = {.x};")) %>%
+        paste(collapse = "\n")
+
+
+      query <- paste(defs, ";", create_statements, query, sep = "\n")
       gor_query(query, conn = conn, relations = virtual_relations)
   }
+
+  structure(fn, class = "gor_creation", defs = defs, dots = dots, conn = conn)
+}
+
+#' @export
+print.gor_creation <- function(x, ...) {
+    x <- attributes(x)
+    if (x$defs == "") x$defs <- "None"
+    bullet <- purrr::partial(cli::cat_bullet, bullet = " ")
+    cli::cat_rule(left = ("GOR Creation Query"))
+
+    cli::cat_line(crayon::green("Connection"))
+    bullet(crayon::blue("Service Root: "), x$conn$service_root)
+    bullet(crayon::blue("Project: "), x$conn$project)
+
+    cli::cat_line(crayon::green("Definitions"))
+    x$defs %>%
+        stringr::str_trim() %>%
+        stringr::str_split("\n", simplify = T) %>%
+        map_chr(stringr::str_trim) %>%
+        paste0(" ", .) %>%
+        map_chr(crayon::italic) %>%
+        walk(cli::cat_line)
+
+    cli::cat_line(crayon::green("Create statements & virtual relations"))
+    x$dots %>%
+        iwalk(function(code, name) {
+            cli::cat_line(" ", crayon::bold(name))
+            cli::cat_line("   ", crayon::italic(as.character(code)))
+        })
 }
