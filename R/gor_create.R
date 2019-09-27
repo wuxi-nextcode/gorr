7399478869e0312068c787d285877bc4d2d27d0d
edvaldig <edvaldig@gmail.com>
2019-09-27 10:28:55 +0000

    vignette created for showcasing gor_create
    gor_creation struct printing fixed

--- R/gor_create.R
+++ R/gor_create.R
@@ -19,52 +19,54 @@
 #' }
 #' @export
 gor_create <- function(..., defs = "", conn = NULL, replace = NULL) {
-  dots <- rlang::dots_list(...)
-  if (!is.null(replace)) {
-    if (class(replace) != "gor_creation") {
-        stop("replace parameter must be of class gor_creation, see gor_create", call. = F)
+    dots <- rlang::dots_list(...)
+    if (!is.null(replace)) {
+        if (class(replace) != "gor_creation") {
+            stop("replace parameter must be of class gor_creation, see gor_create", call. = F)
+        }
+
+        prev <- attributes(replace)
+        if (is.null(conn)) {
+            if (is.null(prev$conn)) stop("conn parameter must be supplied", call. = F)
+            conn <- prev$conn
+        }
+
+        if (defs == "") {
+            defs <- prev$defs
+        }
+
+        prev_names <- purrr::discard(names(prev$dots), ~ . %in% names(dots))
+
+        dots <- c(dots, prev$dots[prev_names])
+
     }
 
-    prev <- attributes(replace)
-    if (is.null(conn)) {
-      conn <- prev$conn
+    dots <- purrr::discard(dots, is.null)
+
+    fn <- function(query) {
+        # evaluating the dots inside this function is necessary, otherwise virtual relations are fixed.
+        relations <- lapply(dots, eval, parent.frame())
+        # Pick out virtual relations
+        virtual_relations <- purrr::keep(relations, is.data.frame)
+        # Pick out create statements
+        create_statements <- relations %>%
+            purrr::keep(is.character) %>%
+            purrr::imap(~ stringr::str_glue("create {.y} = {.x};")) %>%
+            paste(collapse = "\n")
+
+        query <- c(defs, create_statements, query) %>%
+            purrr::discard(function(x) is.null(x) || x == "") %>%
+            paste(collapse = ";\n")
+        gor_query(query, conn = conn, relations = virtual_relations)
     }
 
-    if (defs == "") {
-      defs <- prev$defs
-    }
-
-    prev_names <- purrr::discard(names(prev$dots), ~ . %in% names(dots))
-
-    dots <- c(dots, prev$dots[prev_names])
-
-  }
-
-  dots <- purrr::discard(dots, is.null)
-
-  fn <- function(query) {
-      # evaluating the dots inside this function is necessary, otherwise virtual relations are fixed.
-      relations <- lapply(dots, eval, parent.frame())
-      # Pick out virtual relations
-      virtual_relations <- purrr::keep(relations, is.data.frame)
-      # Pick out create statements
-      create_statements <- relations %>%
-        purrr::keep(is.character) %>%
-        purrr::imap(~ stringr::str_glue("create {.y} = {.x};")) %>%
-        paste(collapse = "\n")
-
-
-      query <- paste(defs, ";", create_statements, query, sep = "\n")
-      gor_query(query, conn = conn, relations = virtual_relations)
-  }
-
-  structure(fn, class = "gor_creation", defs = defs, dots = dots, conn = conn)
+    structure(fn, class = "gor_creation", defs = defs, dots = dots, conn = conn)
 }
 
 #' @export
 print.gor_creation <- function(x, ...) {
     x <- attributes(x)
-    if (x$defs == "") x$defs <- "None"
+
     bullet <- purrr::partial(cli::cat_bullet, bullet = " ")
     cli::cat_rule(left = ("GOR Creation Query"))
 
@@ -73,18 +75,32 @@ print.gor_creation <- function(x, ...) {
     bullet(crayon::blue("Project: "), x$conn$project)
 
     cli::cat_line(crayon::green("Definitions"))
-    x$defs %>%
-        stringr::str_trim() %>%
-        stringr::str_split("\n", simplify = T) %>%
-        purrr::map_chr(stringr::str_trim) %>%
-        paste0(" ", .) %>%
-        purrr::map_chr(crayon::italic) %>%
-        purrr::walk(cli::cat_line)
+    if (x$defs == "") {
+        cli::cat_line("  None")
+    } else {
+        x$defs %>%
+            stringr::str_trim() %>%
+            stringr::str_split("\n", simplify = T) %>%
+            purrr::map_chr(stringr::str_trim) %>%
+            paste0(" ", .) %>%
+            purrr::map_chr(crayon::italic) %>%
+            purrr::walk(cli::cat_line)
+    }
 
     cli::cat_line(crayon::green("Create statements & virtual relations"))
-    purrr::iwalk(x$dots, function(code, name) {
-        cli::cat_line(" ", crayon::bold(name))
-        cli::cat_line("   ", crayon::italic(as.character(code)))
-    })
+    if (length(x$dots) == 0) {
+        cli::cat_line("  None")
+    } else {
+        purrr::iwalk(x$dots, function(code, name) {
+            cli::cat_line(" ", crayon::bold(name))
+
+            if (is.data.frame(code)) {
+                preview <- utils::capture.output(print(code))
+                cli::cat_line("   ", preview)
+            } else {
+                cli::cat_line("   ", crayon::italic(code))
+            }
+        })
+    }
 }
 
