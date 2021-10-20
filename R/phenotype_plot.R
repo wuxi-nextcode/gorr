ffe403906e814dd0848e41d1a48b3bc6dacb80c1
andri@genuitysci.com <7929823-andri8@users.noreply.gitlab.com>
2021-10-20 14:45:55 +0000

    Reformatting and fixing binwidth

--- R/phenotype_plot.R
+++ R/phenotype_plot.R
@@ -5,12 +5,12 @@
 #'
 #' @return ggplot2 plot
 #' @export
-phenotype_plot <- function(phenotype, title=NULL) {
+phenotype_plot <- function(phenotype, title = NULL) {
         assertthat::assert_that(class(phenotype) == "phenotype")
         data <- get_data(phenotype)
         request.fun <- switch(phenotype$result_type, QT = plot__qt, CATEGORY = plot__category, SET = plot__set)
 
-        p <- request.fun(data, colname=phenotype$name, fill="steelblue") + ggplot2::labs(title = if (is.null(title)) phenotype$name else title)
+        p <- request.fun(data, colname=phenotype$name, fill = "steelblue") + ggplot2::labs(title = if (is.null(title)) phenotype$name else title)
     }
 
 
@@ -22,7 +22,7 @@ phenotype_plot <- function(phenotype, title=NULL) {
 #'
 #' @return ggplot2 object
 plot__qt <- function(df, colname, fill) {
-    p <- ggplot2::ggplot(df, ggplot2::aes_string(x=colname)) + ggplot2::geom_histogram(fill=fill)
+    p <- ggplot2::ggplot(df, ggplot2::aes_string(x=colname)) + ggplot2::geom_histogram(fill = fill, binwidth = 0.5)
 }
 
 
@@ -36,9 +36,9 @@ plot__qt <- function(df, colname, fill) {
 plot__category <- function(df, colname, fill) {
     df <- df %>%
         group_by_at(2) %>%
-        summarise(count=n())
-    p <- ggplot2::ggplot(data=df, ggplot2::aes_string(x=colname, y="count")) +
-                ggplot2::geom_bar(stat="identity", fill=fill)
+        summarise(count = n())
+    p <- ggplot2::ggplot(data = df, ggplot2::aes_string(x = colname, y = "count")) +
+                ggplot2::geom_bar(stat = "identity", fill = fill)
 }
 
 
@@ -50,8 +50,8 @@ plot__category <- function(df, colname, fill) {
 #'
 #' @return ggplot2 object
 plot__set <- function(df, colname, fill) {
-    p <- ggplot2::ggplot(df, ggplot2::aes(x=colname)) +
-        ggplot2::geom_bar(fill=fill, width = 0.5) +
-        ggplot2::geom_text(stat='count', ggplot2::aes(label=..count..), vjust=-1) +
+    p <- ggplot2::ggplot(df, ggplot2::aes(x = colname)) +
+        ggplot2::geom_bar(fill = fill, width = 0.5) +
+        ggplot2::geom_text(stat = 'count', ggplot2::aes(label = ..count..), vjust = -1) +
         ggplot2::theme(axis.title.x = ggplot2::element_blank())
 }
