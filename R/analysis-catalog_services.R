0bd9d9e0cb4e60092007f57dff0d55951abda622
andri@genuitysci.com <7929823-andri8@users.noreply.gitlab.com>
2022-07-26 19:56:56 +0200

    Rearranging ac func params to sync with other funs

--- R/analysis-catalog_services.R
+++ R/analysis-catalog_services.R
@@ -1,7 +1,7 @@
 #' A list of all the analysis catalogs in the current project. Optionally scope results to a given phenotype name.
 #'
 #' @param conn gor connection structure, create it using \code{\link{platform_connect}}
-#' @param phenotype_name Only fetch analysis catalogs that relate to a given phenotype name
+#' @param phenotype_name optional: Only fetch analysis catalogs that relate to a given phenotype name
 #' @param limit Maximum number of results fetched (default: 100)
 #'
 #' @return List of analysis catalogs
@@ -42,8 +42,8 @@ get_analysis_catalogs <- function(conn, phenotype_name = NULL, limit = 100) {
 
 #' Get an analysis catalog in the current project by name.
 #'
-#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param analysis_catalog_name The name of the Analysis Catalog
+#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #'
 #' @return analysis_catalog structure
 #' @export
@@ -53,9 +53,9 @@ get_analysis_catalogs <- function(conn, phenotype_name = NULL, limit = 100) {
 #' api_key <- Sys.getenv("GOR_API_KEY")
 #' project <- Sys.getenv("GOR_PROJECT")
 #' conn <- platform_connect(api_key, project)
-#' catalog <- get_analysis_catalog(conn, analysis_catalog_name="test_ac_1")
+#' catalog <- get_analysis_catalog(analysis_catalog_name="test_ac_1", conn = conn)
 #' }
-get_analysis_catalog <- function(conn, analysis_catalog_name) {
+get_analysis_catalog <- function(analysis_catalog_name, conn) {
     assertthat::assert_that(class(conn) == "platform_connection")
 
     url <- gorr__get_endpoint(conn, "phenotype-catalog", "self") %>%
@@ -71,20 +71,20 @@ get_analysis_catalog <- function(conn, analysis_catalog_name) {
 
 #' Create a new analysis catalog in the current project
 #'
-#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param playlist_id The id of the playlist that contains the phenotypes to use for analysis
 #' @param name Analysis Catalog name
 #' @param recipe_name The name of the recipe to use
 #' @param recipe_parameters: The parameters required to run the recipe
+#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param covariate_phenotypes: The names of phenotypes to use as covariates, e.g. ['Pheno1','Pheno2'] (optional)
 #' @param name: excluded_pns: the PNs to exclude from the analysis, e.g. ['PN1','PN2'] (optional)
 #'
 #' @return analysis_catalog structure
-create_analysis_catalog  <- function(conn,
-                                     playlist_id,
+create_analysis_catalog  <- function(playlist_id,
                                      name,
                                      recipe_name,
                                      recipe_parameters,
+                                     conn,
                                      covariate_phenotypes=NULL,
                                      excluded_pns=NULL){
     assertthat::assert_that(class(conn) == "platform_connection")
@@ -116,9 +116,8 @@ create_analysis_catalog  <- function(conn,
 
 #' A list of all the analysis catalog runs in the current project for a given phenotype.
 #'
-#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param phenotype_name Only list analysis catalog runs for a specific phenotype name
-#' @param analysis_catalog_run_name: The name of the Analysis Catalog run
+#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param limit Maximum number of results (default: 100)
 #'
 #' @return List of analysis catalog runs
@@ -129,9 +128,9 @@ create_analysis_catalog  <- function(conn,
 #' api_key <- Sys.getenv("GOR_API_KEY")
 #' project <- Sys.getenv("GOR_PROJECT")
 #' conn <- platform_connect(api_key, project)
-#' catalog <- get_analysis_catalog_runs(conn, phenotype_name = "test_pheno")
+#' catalog <- get_analysis_catalog_runs(phenotype_name = "test_pheno", conn = conn)
 #' }
-get_analysis_catalog_runs <- function(conn, phenotype_name, limit = 100) {
+get_analysis_catalog_runs <- function(phenotype_name, conn, limit = 100) {
     assertthat::assert_that(class(conn) == "platform_connection")
     assertthat::is.string(phenotype_name)
 
@@ -159,9 +158,9 @@ get_analysis_catalog_runs <- function(conn, phenotype_name, limit = 100) {
 
 #' Get an analysis catalog run in the current project by name.
 #'
-#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #' @param analysis_catalog_name The name of the Analysis Catalog
 #' @param analysis_catalog_run_name The name of the Analysis Catalog run
+#' @param conn gor connection structure, create it using \code{\link{platform_connect}}
 #'
 #' @return analysis_catalog_run structure
 #' @export
@@ -171,9 +170,9 @@ get_analysis_catalog_runs <- function(conn, phenotype_name, limit = 100) {
 #' api_key <- Sys.getenv("GOR_API_KEY")
 #' project <- Sys.getenv("GOR_PROJECT")
 #' conn <- platform_connect(api_key, project)
-#' catalog <- get_analysis_catalog_run(conn, analysis_catalog_name="test_ac_1", analysis_catalog_run_name = "test_ac_run_1")
+#' catalog <- get_analysis_catalog_run(analysis_catalog_name="test_ac_1", analysis_catalog_run_name = "test_ac_run_1", conn = conn)
 #' }
-get_analysis_catalog_run <- function(conn, analysis_catalog_name, analysis_catalog_run_name) {
+get_analysis_catalog_run <- function(analysis_catalog_name, analysis_catalog_run_name, conn) {
     assertthat::assert_that(class(conn) == "platform_connection")
     assertthat::is.string(analysis_catalog_name)
     assertthat::is.string(analysis_catalog_run_name)
